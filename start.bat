2415b7fe8daa235fb1ab49091f06bc4383bf5e28
Neon <51766327+DeveloperNeon@users.noreply.github.com>
2021-07-31 12:22:47 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,11 @@
 @echo off
 cls
+npm install discord-rpc
+npm install express
+npm install body-parser
+npm install ejs
+npm install node-fetch
+npm install discord-rich-presence
 title Status Script
 echo Status script starting...
-node .
+node app.js
